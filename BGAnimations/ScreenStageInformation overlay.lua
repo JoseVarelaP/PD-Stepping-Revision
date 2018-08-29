896b6c9eba497aafdc2e767d7d5b8c729eb939b5
Jose_Varela <jo.lu6853@gmail.com>
2018-08-28 19:19:35 -0500

    More stuff for stage info

--- BGAnimations/ScreenStageInformation overlay.lua
+++ BGAnimations/ScreenStageInformation overlay.lua
@@ -30,8 +30,53 @@ t[#t+1] = Def.Sprite {
 	end;
 };
 
+t[#t+1] = Def.Sprite {
+	CurrentSongChangedMessageCommand=function(self)
+	self:stoptweening()
+	self:linear(0.05):diffusealpha(0)
+	self:LoadFromCurrentSongBackground()
+	:queuecommand("UpdateBackground")
+	end,
+	UpdateBackgroundCommand=function(self)
+	if GAMESTATE:GetCurrentSong() then
+		self:LoadFromCurrentSongBackground()
+		self:scale_or_crop_background()
+		self:diffusealpha(0)
+		self:sleep(1)
+		self:decelerate(0.2)
+		self:diffuse(0.7,0.7,0.7,1)
+	end
+	end,
+	OnCommand=function(self)
+		self:scale_or_crop_background()
+		self:diffusealpha(0)
+		self:sleep(1)
+		self:decelerate(0.2)
+		self:diffuse(0.7,0.7,0.7,1)
+	end;
+};
+
 t[#t+1] = Def.Quad{
 	OnCommand=cmd(diffuse,Color.Black;y,SCREEN_BOTTOM-30;horizalign,left;vertalign,bottom;zoomto,500,100;faderight,1);
 }
+
+t[#t+1] = Def.Sprite {
+		InitCommand=cmd(diffusealpha,1;horizalign,left;vertalign,bottom;x,SCREEN_LEFT+10;y,SCREEN_BOTTOM-38);
+		CurrentSongChangedMessageCommand=function(self)
+		self:stoptweening()
+		self:linear(0.05):diffusealpha(0)
+		:queuecommand("UpdateBackground")
+		end,
+		UpdateBackgroundCommand=function(self)
+		if GAMESTATE:GetCurrentSong() then
+			self:LoadFromCurrentSongBackground():diffusealpha(1)
+			self:setsize(170/2,170/2)
+		end
+		end,
+		OnCommand=function(self)
+			self:shadowlength(10):diffusealpha(0):linear(0.5):diffusealpha(1)
+			self:setsize(170/2,170/2)
+		end;
+	};
 
 return t;
