5ae350fc8c01fe61d50e05207285d10a1efe4146
Jose_Varela <jo.lu6853@gmail.com>
2018-09-02 20:15:34 -0500

    some conversion, pretty much

--- BGAnimations/ScreenTitleMenu underlay.lua
+++ BGAnimations/ScreenTitleMenu underlay.lua
@@ -14,17 +14,27 @@ if DIVA:AbleToPlayRandomSongs() and ThemePrefs.Get("EnableRandomSongPlay") then
 	local GlobalItems = cmd(horizalign,left;diffuse,0,0,0,1;zoom,0.6;shadowlengthy,1);
 
 	t[#t+1] = Def.ActorFrame{
-		InitCommand=cmd(diffusealpha,0);
-		OnCommand=cmd(zoom,0.8;x,SCREEN_LEFT+70;y,SCREEN_BOTTOM-35;sleep,.3;decelerate,0.2;diffusealpha,1);
-		OffCommand=cmd(accelerate,0.2;diffusealpha,0);
+		InitCommand=function(self)
+		self:diffusealpha(0)
+		end,
+		OnCommand=function(self)
+		self:zoom(0.8):x(SCREEN_LEFT+70):y(SCREEN_BOTTOM-35):sleep(.3):decelerate(0.2):diffusealpha(1)
+		end,
+		OffCommand=function(self)
+		self:accelerate(0.2):diffusealpha(0)
+		end,
 
 		LoadActor( THEME:GetPathG("","TitleMenu/MusicNote"))..{
-		OnCommand=cmd(zoom,0.15;x,-15;y,-3);
+		OnCommand=function(self)
+		self:zoom(0.15):x(-15):y(-3)
+		end,
 		};
 
 		LoadFont("Common Normal")..{
 		Text=DIVA_RandomSong:GetDisplayFullTitle() .." - ".. DIVA_RandomSong:GetDisplayArtist();
-		InitCommand=GlobalItems;
+		InitCommand=function(self)
+		self:horizalign(left):diffuse(0,0,0,1):zoom(0.6):shadowlengthy(1)
+		end;
 		OnCommand=function(self)
 		self:strokecolor( Color.White )
 		local ToMove = -2
