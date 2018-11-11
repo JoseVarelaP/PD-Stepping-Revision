79a457f920ce71df73a6edf158ff897773436f0e
Jose_Varela <jo.lu6853@gmail.com>
2018-11-11 14:05:06 -0600

    Make Character Stage become the selected location

--- BGAnimations/DivaRoom overlay/MainMenu overlay.lua
+++ BGAnimations/DivaRoom overlay/MainMenu overlay.lua
@@ -8,6 +8,7 @@ local ChoicesList = {
     { function() SCREENMAN:AddNewScreenToTop("DivaRoom overlay/LocaChooser", "SM_GoToNextScreen") end,"Switch Location" },
     { function()
         GAMESTATE:SetCharacter(PLAYER_1, getenv("DivaRoom_CharLoad"):GetCharacterID())
+        ThemePrefs.Set("CurrentStageLocation", getenv("DivaRoom_LocaLoad") )
         setenv( "DivaRoomNextScreen", "ScreenSelectMusic" )
         SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
     end,"Use as Gameplay Character\n(This will start the game as player 1!)" },
